e607495cf30ac29745bbb155333d54d8b0812d94
Leonardo Freitas <leonardoft9@hotmail.com>
2020-12-01 21:40:50 -0300

    implementação do UsuarioServiceImpl.java

---
+++ src/main/java/com/leo/financeiro/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,33 @@
+package com.leo.financeiro.service.impl;
+
+import com.leo.financeiro.model.entity.Usuario;
+import com.leo.financeiro.model.repository.UsuarioRepository;
+import com.leo.financeiro.service.UsuarioService;
+
+public class UsuarioServiceImpl implements UsuarioService {
+	
+	private UsuarioRepository repository;
+	
+	public UsuarioServiceImpl(UsuarioRepository repository) {
+		super();
+		this.repository = repository;
+	}
+
+	@Override
+	public Usuario autenticar(String email, String senha) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public Usuario salvarUsuario(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void validarEmail(String email) {
+		// TODO Auto-generated method stub
+		
+	}
+}
