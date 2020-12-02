2db60122cea7f63ed744f079bfcef384c7f59ea2
Leonardo Freitas <leonardoft9@hotmail.com>
2020-12-02 12:27:29 -0300

    implementação do @DataJpaTest

--- src/test/java/com/leo/financeiro/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/leo/financeiro/model/repository/UsuarioRepositoryTest.java
@@ -4,14 +4,14 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.leo.financeiro.model.entity.Usuario;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@DataJpaTest
 @ActiveProfiles("test")
 public class UsuarioRepositoryTest {
 	
