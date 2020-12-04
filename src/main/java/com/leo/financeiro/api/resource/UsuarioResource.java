28372dd63866c4082795e0edb85a799d2e522c4e
Leonardo Freitas <leonardoft9@hotmail.com>
2020-12-04 14:54:53 -0300

    Implementa UsuarioDTO.java e UsuarioResource.java

---
+++ src/main/java/com/leo/financeiro/api/resource/UsuarioResource.java
@@ -0,0 +1,43 @@
+package com.leo.financeiro.api.resource;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.leo.financeiro.api.dto.UsuarioDTO;
+import com.leo.financeiro.exception.RegraNegocioException;
+import com.leo.financeiro.model.entity.Usuario;
+import com.leo.financeiro.service.UsuarioService;
+
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioResource {
+	
+	private UsuarioService service;
+	
+	public UsuarioResource( UsuarioService service ) {
+		this.service = service;
+	}
+	
+	@PostMapping
+	public ResponseEntity salvar( @RequestBody UsuarioDTO dto) {
+		
+		Usuario usuario = Usuario.builder()
+				.nome(dto.getNome())
+				.email(dto.getEmail())
+				.senha(dto.getSenha())
+				.build();
+		
+		try {
+			Usuario usuarioSalvo = service.salvarUsuario(usuario);
+			return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
+		}catch (RegraNegocioException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());		
+		}
+	}
+	
+}
