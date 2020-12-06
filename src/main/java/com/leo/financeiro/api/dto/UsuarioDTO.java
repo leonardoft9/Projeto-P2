8e64d81ace4e894be98683ae93486dd2609d6631
Leonardo Freitas <leonardoft9@hotmail.com>
2020-12-06 05:41:49 -0300

    corrige UsuarioDTO.java

--- src/main/java/com/leo/financeiro/api/dto/UsuarioDTO.java
+++ src/main/java/com/leo/financeiro/api/dto/UsuarioDTO.java
@@ -1,13 +1,16 @@
 package com.leo.financeiro.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UsuarioDTO {
 
 	private String email;
